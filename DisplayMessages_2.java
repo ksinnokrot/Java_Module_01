53e684b1643bd99cb1c68a7bd483e20ba8eb51e0
ksinnokrot <151904136+ksinnokrot@users.noreply.github.com>
2023-12-05 06:24:16 +0500

    Add files via upload

---
+++ DisplayMessages_2.java
@@ -0,0 +1,8 @@
+public class DisplayMessages_2 {
+    public static void main(String[] args) {
+        // Displaying "Welcome to Java" five times
+        for (int i = 0; i < 5; i++) {
+            System.out.println("Welcome to Java");
+        }
+    }
+}
